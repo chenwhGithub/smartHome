1fcfd1ee0d4a24e9360286eee7f4244d6dc36e8f
wenhchen <wenhui.chen@nokia-sbell.com>
2019-12-20 22:00:24 +0800

    change command to Upter

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def saveAndSendImage(sender):
 
 def saveAndSendVideo(sender, mSecond):
     fileName = camera.Camera_saveVideo(mSecond)
-    itchat.send('@fil@%s' %fileName, toUserName=sender)
+    itchat.send('@vid@%s' %fileName, toUserName=sender)
     print("send success %s" %fileName)
 
 def motorForwardThread(angle):
@@ -37,23 +37,23 @@ def motorBackwardThread(angle):
 def text_reply(msg):
     recvMsg = msg.text
     sender = msg['FromUserName']
-    if recvMsg == "p":
+    if (recvMsg == "P") or (recvMsg == "p"):
         saveAndSendImage(sender)
-    elif recvMsg == "v":
-        itchat.send('please wait, videoing...', toUserName=sender)
+    elif (recvMsg == "V") or (recvMsg == "v"):
+        itchat.send('please wait...', toUserName=sender)
         saveAndSendVideo(sender, 5000)
     else:
-        pattern = re.compile(r"(l|r)([1-9][0-9]?)\Z") # l50 or r30
+        pattern = re.compile(r"(L|l|R|r)([1-9][0-9]?)\Z") # l50 or r30
         matchObjs = pattern.match(recvMsg)
         if matchObjs:
             angle = int(matchObjs.group(2))
-            if matchObjs.group(1) == "l":
+            if (matchObjs.group(1) == "l") or (matchObjs.group(1) == "L"):
                 t1 = threading.Thread(target=motorForwardThread, args=(angle,))
                 t1.start()
             else:
                 t2 = threading.Thread(target=motorBackwardThread, args=(angle,))
                 t2.start()
-            itchat.send('please wait, rotating...', toUserName=sender)
+            itchat.send('please wait...', toUserName=sender)
             saveAndSendVideo(sender, angle*115) # 0.02*4*(angle/0.7)*1000
 
 def itChatThread():
